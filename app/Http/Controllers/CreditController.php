b4aa5c27c2d954005c927e5264c25668f2d6bc5a
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2024-12-26 23:14:58 +0000

    working on stripe integration

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\Feature\FeatureResource;
 use App\Http\Resources\PackageResource;
 use App\Models\Feature\Feature;
 use App\Models\Package;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class CreditController extends Controller
@@ -17,22 +18,51 @@ class CreditController extends Controller
         return inertia('Credit/Index', [
             'packages' => PackageResource::collection($packages),
             'features' => FeatureResource::collection($features),
+            'success' => session('success'),
+            'error' => session('error')
         ]);
     }
 
-    public function buyCredits(){
+    public function buyCredits(Package $package){
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+        $checkout_session = $stripe->checkout->sessions->create([
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => $package->name . ' - ' . $package->credits . ' credits',
+                        ],
+                        'unit_amount' => $package->price * 100,
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+            'success_url' => route('credit.success', [], true),
+        ]);
 
+        Transaction::create([
+            'status' => 'pending',
+            'price' => $package->price,
+            'credits' => $package->credits,
+            'session_id' => $checkout_session->id,
+            'user_id' => auth()->id(),
+            'package_id' => $package->id
+        ]);
+
+        return redirect($checkout_session->url);
     }
 
     public function success(){
-
+        return to_route('credit.index')->with('success', 'You have successfully bought new credit');
     }
 
     public function cancel(){
-
+        return to_route('credit.index')->with('error', 'There was an error making payment, try again later');
     }
 
     public function webhook(){
-
+        $endpoint_secret = env('STRIPE_WEBHOOK_KEY');
     }
 }
